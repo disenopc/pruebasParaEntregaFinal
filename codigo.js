12668f9516d5190a0d8fbb7d00452fda0d24d897
disenopc <capitanellipl@gmail.com>
2022-07-07 21:01:42 -0300

    evento de input

--- codigo.js
+++ codigo.js
@@ -1,23 +1,22 @@
-const dialog = document.querySelector("dialog");
-const cancel = document.querySelector("#cancel");
-const show = document.querySelector("#show");
+// const dialog = document.querySelector("dialog");
+// const cancel = document.querySelector("#cancel");
+// const show = document.querySelector("#show");
 
-show.addEventListener("click", () => dialog.showModal());
-cancel.addEventListener("click", () => dialog.close());
+// show.addEventListener("click", () => dialog.showModal());
+// cancel.addEventListener("click", () => dialog.close());
 
-//PASOS
-//ESCRIBIR EL INPUT Y ENVIARLO
 
-//RECIBO LA INFO DEL INPUT
 
-let codigoPostal = document.querySelector("#cP");
-codigoPostal.addEventListener("click", () => retiro.showModal());
 
-let retiro = document.querySelector("#retiroModal");
-const cerrarM = document.querySelector("#cerrar");
-cerrarM.addEventListener("click", () => retiro.close());
 
-let sucursalCerca = "";
+// let codigoPostal = document.querySelector("#cP");
+// codigoPostal.addEventListener("click", () => retiro.showModal());
+
+// let retiro = document.querySelector("#retiroModal");
+// const cerrarM = document.querySelector("#cerrar");
+// cerrarM.addEventListener("click", () => retiro.close());
+
+// let sucursalCerca = "";
 
 
 
@@ -36,69 +35,12 @@ function obtenerDomicilio() {
 };
 
 obtenerDomicilio();
-let sucursales = [];
-let coincideCp = document.querySelector("#inputCp").value;
-coincideCp.addEventListener("input", updateValue);
+//PASOS
+//ESCRIBIR EL INPUT Y ENVIARLO
+//RECIBO LA INFO DEL INPUT
+const input = document.querySelector('#inputCp');
+input.addEventListener('input', updateValue);
 
-function updateValue(codigoPostal) {
-    console.log(codigoPostal.target.value);
-
+function updateValue(e) {
+    console.log(e.target.value);
 }
-// while (coincideCp != "") {
-//     const codePost = sucursales.filter(sucursales => sucursales.codigoPostal == coincideCp);
-//     console.log(codePost);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// buscarElCpEnData();
-
-//BUSCAR EN EL ARRAY EL CODIGO POSTAL
-
-
-// function buscarElCpEnData() {
-//     
-//     console.log(codePost);
-// }
-
-
-
-
-
-
-//LUEGO DEVOLVER EL DOMICILIO  DE ESE CODIGO POSTAL
-// const modalDelDom = document.querySelector(".modalDePrueba");
-// const modalEnElDom = (sucursales) => {
-//     const modal = document.createElement("div");
-//     modal.classList.add("modal-content");
-//     modal.innerHTML = `
-//                 <div class="modal" tabindex="-1">
-//                 <div id="modalDePrueba" class="modal-dialog">
-//                 <div class="modal-header">
-//                     <h5 class="modal-title">Modal title</h5>
-//                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//                 </div>
-//                 <div class="modal-body">
-//                     <p>Modal body text goes here.</p>
-//                 </div>
-//                 <div class="modal-footer">
-//                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//                     <button type="button" class="btn btn-primary">Save changes</button>
-//                 </div>
-//                 </div>
-//                 </div>
-
-//     `
-//     modalDelDom.appendChild(modal)
-// }
